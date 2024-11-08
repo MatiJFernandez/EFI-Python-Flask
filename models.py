79fa96244ef64cb0b19ced097e85e51931aa61fd
MatiJFernandez <mj.fernandez@itecriocuarto.org.ar>
2024-11-07 21:23:11 -0300

    agrega modelos (stock, accesorios, celular_accesorios,)

--- models.py
+++ models.py
@@ -19,11 +19,30 @@ class Celular(db.Model):
     
     #Relacion directa con el otro objeto
     marca = db.relationship('Marca', backref=db.backref('vehiculos', lazy=True))
-    
 
+    
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False)
     password_hash = db.Column(db.String(300), nullable=False)
-    is_admin = db.Column(db.Boolean(0))
+    is_admin = db.Column(db.Boolean(0), default=False)
+
+class Accesorio(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+
+    celulares = db.relationship('Celular_Accesorio', back_populates='accesorio', lazy=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Celular_Accesorio(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    telefono_id = db.Column(db.Integer, db.ForeignKey('celular.id'), nullable=False)
+    accesorio_id = db.Column(db.Integer, db.ForeignKey('accesorio.id'), nullable=False)
+
+    accesorio = db.relationship('Accesorio', back_populates='celulares')
+
+    def __str__(self):
+        return f"Accesorio {self.accesorio.nombre} para el celular {self.celular.modelo}"
 
