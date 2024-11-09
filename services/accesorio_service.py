86140d445a949c350db85a925d14ee5356ded635
MatiJFernandez <mj.fernandez@itecriocuarto.org.ar>
2024-11-09 01:32:00 -0300

    actualiza accesorios_service

--- services/accesorio_service.py
+++ services/accesorio_service.py
@@ -1,20 +1,20 @@
 from repositories.accesorio_repositories import AccesorioRepositories
 
 class AccesorioService:
-    def __init__(self):
-        self.repository = AccesorioRepositories()
+    def __init__(self, accesorio_repository: AccesorioRepositories):
+        self._accesorio_repository = accesorio_repository
 
     def get_all(self):
-        return self.repository.get_all()
+        return self._accesorio_repository.get_all()
 
     def get_by_id(self, id):
-        return self.repository.get_by_id(id)
+        return self._accesorio_repository.get_by_id(id)
 
     def create(self, nombre):
-        self.repository.create(nombre)
+        self._accesorio_repository.create(nombre)
 
     def update(self, id, nombre):
-        self.repository.update(id, nombre)
+        self._accesorio_repository.update(id, nombre)
 
     def delete(self, id):
-        self.repository.delete(id)
+        self._accesorio_repository.delete(id)
